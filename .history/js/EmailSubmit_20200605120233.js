99a0119b7ecae88f88cf366a086b08be73cde921
Kweku Martin <kweku.martin@outlook.com>
2020-06-05 13:26:52 -0400

    Updating Website

---
+++ .history/js/EmailSubmit_20200605120233.js
@@ -0,0 +1,28 @@
+
+$(function() {
+    
+
+// Set up an event listener for the contact form.
+$("#canvasF").submit(function(event) {
+    // Stop the browser from submitting the form.
+    event.preventDefault();
+
+    // TODO
+
+    var element = document.getElementById('email').value;
+    var forms = { "Email": element}
+    $.ajax({
+            type: "post",
+            url: "https://greekpeekapi.azurewebsites.net/api/Prerollment/EnrollForUpdate",
+            data: JSON.stringify(forms),
+            contentType: "application/json",
+            success: function(responseData, textStatus, jqXHR) {
+              $('#myModal').modal('show');
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(errorThrown);
+            }
+        })
+})
+
+});
